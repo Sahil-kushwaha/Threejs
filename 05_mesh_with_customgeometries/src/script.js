b767c41e51c55850ab53a85a5e2b434167fc9583
sahil kushwaha <sahilkushwaha5510@gmail.com>
2024-10-16 23:57:22 +0530

    creating custom geometry with BufferGeometry class

---
+++ 05_mesh_with_customgeometries/src/script.js
@@ -0,0 +1,63 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+// initialize the scene
+const scene = new THREE.Scene();
+
+// add objects to the scene
+// const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+// custom geometry 
+// create typeArray to store vertices position Buffergemometry(2d triangle)
+const vertices = new Float32Array([
+   0,0,0,
+   0,2,0,
+   2,0,0
+])
+// (x,y,z) itemSize(the no of value of array associated with vertex) is 3 here 
+const bufferattribute = new THREE.BufferAttribute(vertices,3)
+
+const geometry = new THREE.BufferGeometry()
+geometry.setAttribute('position',bufferattribute)
+
+const triangleMaterial = new THREE.MeshBasicMaterial({ color: "red", wireframe: true });
+const triangleMesh = new THREE.Mesh(geometry,triangleMaterial)
+scene.add(triangleMesh)
+
+// initialize the camera
+const camera = new THREE.PerspectiveCamera(
+  35,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  200
+);
+camera.position.z = 5;
+
+// initialize the renderer
+const canvas = document.querySelector("canvas.threejs");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// instantiate the controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// render the scene
+const renderloop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(renderloop);
+};
+
+renderloop();
